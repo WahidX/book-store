cb1347b19a282d2b5b16499b1d4f80e4d5368e24
WahidX <wahidx93@gmail.com>
2021-01-19 00:19:52 +0530

    file structure created

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import App from "./App";
+// import Footer from './Footer';
+// import Home from './Home';
+// import Page404 from './Page404';
+// import Profile from './Profile';
+// import CustomizedSnackbars from './SnackBar';
+
+export {
+	App,
+	//   Footer,
+	//   Home,
+	//   Page404,
+	//   CustomizedSnackbars,
+};
